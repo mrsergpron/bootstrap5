8f1516784545c73f70a4cb88e4fb62601811234e
sergei <spron2014@yandex.ru>
2021-08-30 20:14:05 +0300

    category

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@ $(function () {
     event.preventDefault();
     $("#search").removeClass("active");
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $("#top").fadeIn();
+    } else {
+      $("#top").fadeOut();
+    }
+  });
+
+  $("#top").click(function () {
+    $("body, html").animate({ scrollTop: 0 }, 700);
+  });
+
+  $(".sidebar-toggler .btn").click(function () {
+    $(".sidebar-toggle").slideToggle();
+  });
 });
